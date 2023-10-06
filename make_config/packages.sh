e16b7ff5417dd6acaed96684190dcb7b661c85ae
areif-dev <ajreifsnyder@protonmail.com>
2023-10-05 21:01:32 -0400

    Start script to automate install

---
+++ make_config/packages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+std_packages=(
+    alacritty
+    base-devel
+    discord 
+    firefox
+    grim
+    libreoffice-fresh
+    neovim
+    networkmanager
+    openssh
+    pavucontrol
+    river
+    swayidle
+    swaylock
+    torbrowser-launcher 
+    waybar
+    xorg-xwayland
+    zsh
+)
+
+aur_packages=(
+    freetube-bin 
+    rofi-lbonn-wayland
+)
+
