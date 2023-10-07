64fc1bd003bd1cc7cadc97e783c7d0f1cb44d925
areif-dev <ajreifsnyder@protonmail.com>
2023-10-07 12:33:58 -0400

    Allow parallel downloads for pacman

--- make_config/packages.sh
+++ make_config/packages.sh
@@ -25,3 +25,8 @@ aur_packages=(
     rofi-lbonn-wayland
 )
 
+# Enable 5 parallel downloads at a time in pacman 
+sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
+
+pacman -Syu
+pacman -S $std_packages
