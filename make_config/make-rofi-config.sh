d35dfaf379b4cd98abb7378ee6979c6ed9816cac
areif-dev <ajreifsnyder@protonmail.com>
2023-10-11 19:34:25 -0400

    Use variables names window_width and window_height in rofi config

--- make_config/make-rofi-config.sh
+++ make_config/make-rofi-config.sh
@@ -7,5 +7,5 @@ rofi_side_px=$(printf "%.0f" $(echo "$display_height_px * 0.65" | bc))
 
 cp $config_dir/rofi/config.rasi.bak $config_dir/rofi/config.rasi
 
-sed -i "s/width.*/width: ${rofi_side_px}px;/" $config_dir/rofi/config.rasi
-sed -i "s/height.*/height: ${rofi_side_px}px;/" $config_dir/rofi/config.rasi
+sed -i "s/window_width/width: ${rofi_side_px}px;/" $config_dir/rofi/config.rasi
+sed -i "s/window_height/height: ${rofi_side_px}px;/" $config_dir/rofi/config.rasi
