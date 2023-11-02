2a99796e980d5b6cd4151c929d7c09ce1805d2f8
areif-dev <ajreifsnyder@protonmail.com>
2023-11-02 14:59:25 -0400

    Fix looping through displays in monitor-scale.sh

--- river/monitor-scale.sh
+++ river/monitor-scale.sh
@@ -11,19 +11,15 @@ display_sizes=($(wlr-randr | awk '/Physical size:/ {split($3,a,"x"); print a[1]}
 # Select the width in pixels (eg. 1920)
 display_dots=($(wlr-randr | awk '/current/ {split($1,a,"x"); print a[1]}'))
 
-for i in $(seq 0 ${#display_adapters[@]})
+for i in "${!display_adapters[@]}"
 do 
-
-  # Reached the end of the array, so end the loop
-  if [ $i == ${#display_adapters[@]} ]; then
-    break 
-  fi 
-
   # Calculate dots per mm then convert to dots per inch
   dpi=$(echo "scale=2; ${display_dots[$i]} / ${display_sizes[$i]} * $MM_PER_INCH" | bc)
 
   # Assume that the scale of the display should be the nearest multiple of 100
   # Eg. 108 DPI would be a display scale of 1. 267 DPI is scale of 2.
   scale=$(echo "scale=0; $dpi / 100" | bc)
-  exec wlr-randr --output ${display_adapters[$i]} --scale $scale
+  scale=$((scale>1 ? scale : 1))
+
+  wlr-randr --output ${display_adapters[$i]} --scale $scale
 done
